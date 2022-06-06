bbd065e21179774da259ca8b68d49a43690d76a0
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-06-06 16:26:29 +0000

    タスク投稿機能

--- app/controllers/public/tasks_controller.rb
+++ app/controllers/public/tasks_controller.rb
@@ -4,7 +4,7 @@ class Public::TasksController < ApplicationController
     @task = Task.new(task_params)
     @task.customer_id = current_customer.id
     @task.save
-    redirect_to request.referer
+    redirect_to request.referer, notice: "タスクを投稿しました！頑張りましょう！"
   end
 
   def index
@@ -14,15 +14,31 @@ class Public::TasksController < ApplicationController
   end
 
   def show
+    @task = Task.find(params[:id])
+    # ステータス毎に文字色を変更する為のクラスをあてがう
+    @status = @task.progress_status
+    # 日本語化の表記
+    @status_i18n = @task.progress_status_i18n
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task), notice: "進捗を更新しました"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    if @task.destroy
+      redirect_to tasks_path, notice: "タスクを削除しました"
+    end
   end
 
   private
