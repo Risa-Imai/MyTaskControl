b62ebe0f0fb499cadd9124691e3455c15cadb87b
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-06-16 02:08:05 +0000

    フォローフォロワー一覧

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -13,11 +13,11 @@ class Public::RelationshipsController < ApplicationController
   # フォロー一覧
   def followings
     @customer = Customer.find(params[:customer_id])
-    @customers = customer.followings
+    @customers = @customer.followings
   end
   # フォロワー一覧
   def followers
     @customer = Customer.find(params[:customer_id])
-    @customers = customer.followers
+    @customers = @customer.followers
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'relationships/followings'
-    get 'relationships/followers'
-  end
   # 顧客用
   # URL /customers/dign_in ...
   devise_for :customers, skip: [:passwords], controllers: {
