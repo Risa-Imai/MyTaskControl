cd458e0f4cf046029790d879990054de7ca3badc
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-06-18 07:56:01 +0000

    fixed for task_tags bigint issue

--- db/migrate/20220611104116_create_task_tags.rb
+++ db/migrate/20220611104116_create_task_tags.rb
@@ -1,8 +1,8 @@
 class CreateTaskTags < ActiveRecord::Migration[6.1]
   def change
     create_table :task_tags do |t|
-      t.references :task, null: false, foreign_key: true
-      t.references :tag, null: false, foreign_key: true
+      t.integer :task, null: false, foreign_key: true
+      t.integer :tag, null: false, foreign_key: true
 
       t.timestamps
     end
