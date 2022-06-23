6d5f98ad700f62841ed1e10575f918f137166967
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-06-23 07:04:29 +0000

    [Add]会員モデルRSpec追記

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -9,9 +9,29 @@ RSpec.describe Customer, type: :model do
          last_name: "tanaka",
       introduction: "田中太郎と申します。よろしくお願いします。",
              email: "tanaka@example.com",
-          password: "password",
+          password: "password"
       )
     expect(customer).to be_valid
   end
 
+  it "姓、名を登録すると、姓名が取得できること" do
+    customer = Customer.new(
+         last_name: "tanaka",
+        first_name: "tarou"
+    )
+    expect(customer.full_name).to eq "tanaka tarou"
+  end
+
+  it "名がない場合、無効である" do
+    # first_nameでnilを設定する
+    customer = Customer.new(
+        first_name: nil,
+         last_name: "tanaka",
+      introduction: "田中太郎と申します。よろしくお願いします。",
+             email: "tanaka@example.com",
+          password: "password"
+        )
+    customer.valid?
+    expect(customer.errors[:first_name]).to include("を入力してください", "は1文字以上で入力してください")
+  end
 end
