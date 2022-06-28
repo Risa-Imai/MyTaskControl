d943dc076618c10bb01f8bfd2908e9672c121272
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-06-28 02:04:16 +0000

    [Fix]task#indexでtagの表示多い順

--- app/controllers/admin/tasks_controller.rb
+++ app/controllers/admin/tasks_controller.rb
@@ -3,7 +3,8 @@ class Admin::TasksController < ApplicationController
 
   def index
     @tasks = Task.latest.page(params[:page])
-    @tag_list = Tag.all
+    @tag_list = Tag.find(TaskTag.group(:tag_id).order("count(task_id) desc").limit(25).pluck(:tag_id))
+    # @tag_list = Tag.all
   end
 
   def show
@@ -16,19 +17,21 @@ class Admin::TasksController < ApplicationController
       redirect_to admin_tasks_path, alert: "#{@task.customer.full_name}さんのタスクを削除しました"
     end
   end
-  
+
   def search
     @tasks = Task.search(params[:keyword]).latest.page(params[:page])
     # renderしているのでpageメソッドを渡している
     @keyword = params[:keyword]
-    @tag_list = Tag.all
+    @tag_list = Tag.find(TaskTag.group(:tag_id).order("count(task_id) desc").limit(25).pluck(:tag_id))
+    # @tag_list = Tag.all
     render :index
   end
 
   def tag_search
     @tag = Tag.find(params[:tag_id])
     @tasks = @tag.tasks.latest.page(params[:page])
-    @tag_list = Tag.all
+    @tag_list = Tag.find(TaskTag.group(:tag_id).order("count(task_id) desc").limit(25).pluck(:tag_id))
+    # @tag_list = Tag.all
     # @tasks = Task.includes(:tags).where(tags: {id: @tag}).page(params[:page])
     render :index
   end
