e8f69334e5fb135f1b3e077a157f247ad8e3e0d6
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-07-12 02:19:48 +0000

    [Add]Oauth2 SNS認証

--- app/controllers/public/omniauth_callbacks_controller.rb
+++ app/controllers/public/omniauth_callbacks_controller.rb
@@ -1,6 +1,22 @@
 # frozen_string_literal: true
 
 class Public::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    # callback for google
+  def google_oauth2
+    callback_for(:google)
+  end
+
+  def callback_for(provider)
+    # 先ほどcustomer.rbで記述したメソッド(from_omniauth)をここで使っています
+    # "request.env["omniauth.auth"]"この中にgoogoleアカウントから取得したメールアドレスや、名前と言ったデータが含まれています
+    @customer = Customer.from_omniauth(request.env["omniauth.auth"])
+    sign_in_and_redirect @customer, event: :authentication
+    set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+  end
+
+  def failure
+    redirect_to request.referer
+  end
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
 
